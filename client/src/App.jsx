398083f722d1f6446e6613f933c77d930c01d4ce
Peter <dayr88520@gmail.com>
2024-04-09 00:03:59 +0800

    新增components 並引入router套件

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+
+// import React router dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Dashboard />
+      <Login />
+      <Register />
+    </>
+  );
 }
 
 export default App;
